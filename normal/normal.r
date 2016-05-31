ba726b09f8316ff744acef02c987c62ce67256e2
eki1381 <eki1381@gmail.com>
2016-05-31 13:14:06 +0700

    Ada warnanya, bagus!!!

--- normal/normal.r
+++ normal/normal.r
@@ -1,8 +1,17 @@
 norm <- function(n,mean,sd,nos){
+  means <- rep(NA,nos)
+  
   for(i in 1:nos){
-    mean[i] = mean(rnorm(n = n,mean = mean,sd = sd))
+    means[i] = mean(rnorm(n = n,mean = mean,sd = sd))
   }
-  hist(mean , main = paste("Histogram distribusi"), prob = T)
-  lines(density(mean))
-  lines(x = sort(mean), y = dnorm(sort(mean), mean(mean), sd(mean)), col = "red")
+  
+  meanss <- as.data.frame(means)
+  
+  ggplot(data=meanss, aes(x=means)) + geom_histogram(aes(y=..density..,fill=..density..),binwidth = 0.2) + geom_density() +
+    stat_function(fun=dnorm,color="red",args=list(mean=mean(means),sd=sd(means)))
+  
+  #hist(means,main = paste("Histogram distribusi"),breaks=5, prob = T)
+  
+  #lines(density(means))
+  #lines(x = sort(means), y = dnorm(sort(means), mean(means), sd(means)), col = "red")
 }
